72f409a326206db88bf56e46083de11579aa47ec
abby <ba40431@gmail.com>
2023-04-20 22:41:06 +0800

    init

---
+++ routes/module/point.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const PointController = require('../../controllers/pointController')
+// const pointController = new PointController()
+
+// router.get('/', pointController.testify)
+
+module.exports = router
